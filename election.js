581c4156200dcc1a65e5bd66bf281c093ffe06a5
Paul Walsh <twentytwentyk@live.com>
2017-02-21 15:36:45 -0500

    done

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+    $.ajax({
+      url: "https://bb-election-api.herokuapp.com/",
+      method: "GET",
+      dataType: "json"
+    }).done(function(responseData) {
+      for (i = 0; i < responseData.candidates.length; i++) {
+      var content = $('<li>').html(responseData.candidates[i].name + " Votes: " + responseData.candidates[i].votes)
+      $("#characters").append(content)
+    };
 
-  // Imagination!
+
+  });
 
 });
